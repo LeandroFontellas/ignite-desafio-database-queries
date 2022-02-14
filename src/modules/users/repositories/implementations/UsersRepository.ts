f1516b42514bb26f0de2d9d576f6f910f9647156
Leandro <leandrofontellas@gmail.com>
2022-02-14 10:39:10 -0300

    feat(users): :sparkles: usersrep 100%

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,24 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return this.repository.findOneOrFail({where:{id:user_id},relations:["games"]})
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY users.first_name"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    // Provavelmente tem um jeito mais simples de fazer isso.
+    let [firstChar1,rest1] = first_name.split(first_name.charAt(0),2);
+    firstChar1= first_name.charAt(0).toUpperCase()
+    rest1=rest1.toLowerCase();
+    let [firstChar2,rest2] = last_name.split(last_name.charAt(0),2);
+    firstChar2= last_name.charAt(0).toUpperCase()
+    rest2=rest2.toLowerCase()
+    return this.repository.query(`SELECT * FROM users WHERE users.first_name = $1 AND users.last_name = $2`,[firstChar1+rest1,firstChar2+rest2]); // Complete usando raw query
   }
 }
