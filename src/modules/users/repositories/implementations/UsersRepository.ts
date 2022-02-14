311372afff5d2142e81634972bd075ea343c0448
Leandro <leandrofontellas@gmail.com>
2022-02-14 11:12:17 -0300

    fix(simplify): :bug: simplify code

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -25,13 +25,6 @@ export class UsersRepository implements IUsersRepository {
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    // Provavelmente tem um jeito mais simples de fazer isso.
-    let [firstChar1,rest1] = first_name.split(first_name.charAt(0),2);
-    firstChar1= first_name.charAt(0).toUpperCase()
-    rest1=rest1.toLowerCase();
-    let [firstChar2,rest2] = last_name.split(last_name.charAt(0),2);
-    firstChar2= last_name.charAt(0).toUpperCase()
-    rest2=rest2.toLowerCase()
-    return this.repository.query(`SELECT * FROM users WHERE users.first_name = $1 AND users.last_name = $2`,[firstChar1+rest1,firstChar2+rest2]); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users WHERE users.first_name ILIKE $1 AND users.last_name ILIKE $2`,[first_name,last_name]); // Complete usando raw query
   }
 }
